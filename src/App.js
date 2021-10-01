ed09a69fa457df2d07500ebbf1f8b7508f0a330f
AHILYND <AHILYND@UDEL.EDU>
2021-09-30 23:06:28 -0400

    created figure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Figure from './components/Figure';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <h1>Hangman</h1>
+            <p>What's The Hidden Word - Start guessing</p>
+        </>
+    )
+}
+
+export default Header
