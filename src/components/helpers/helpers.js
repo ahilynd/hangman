c4f2ceb18efaad01ff9008a32954bba2ffb6e09b
AHILYND <AHILYND@UDEL.EDU>
2021-09-30 23:23:19 -0400

    new files/folders and win lose situations

---
+++ src/components/helpers/helpers.js
@@ -0,0 +1,20 @@
+export function showNotification(setter){
+    setter(true);
+    setTimeout(() => {
+        setter(false);
+    }, 4000);
+}
+
+export function checkWin(correct, wrong, word){
+    let status = 'win';
+
+    word.split('').forEach(letter => {
+        if(!correct.includes(letter)){
+            status = '';
+        }
+    });
+
+    if(wrong.length === 5) status = 'lose';
+
+    return status
+}
